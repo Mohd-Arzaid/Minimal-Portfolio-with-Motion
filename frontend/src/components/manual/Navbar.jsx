91ea760363328a85309d528210b2ca905e38004c
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-05-30 01:00:44 +0530

    fix navbar

--- frontend/src/components/manual/Navbar.jsx
+++ frontend/src/components/manual/Navbar.jsx
@@ -1,7 +1,7 @@
 import Container from "./Container";
 import ProfileImage from "../../assets/heroportfolio.jpg";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 import { useState } from "react";
 
 const Navbar = () => {
@@ -25,10 +25,32 @@ const Navbar = () => {
   ];
 
   const [hovered, setHovered] = useState(null);
+  const { scrollY } = useScroll();
+  const [scrolled, setScrolled] = useState(false);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    // console.log("scrollY",latest);
+    if (latest > 20) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  });
 
   return (
     <Container>
-      <nav className="flex items-center justify-between p-2 ">
+      <motion.nav
+        animate={{
+          boxShadow: scrolled ? "var(--shadow-input)" : "none",
+          width: scrolled ? "50%" : "100%",
+          y: scrolled ? 10 : 0,
+        }}
+        transition={{
+          duration: 0.3,
+          ease: "linear",
+        }}
+        className="flex fixed top-0 inset-x-0 z-50 max-w-4xl mx-auto items-center justify-between rounded-full px-3 py-2 bg-white dark:bg-neutral-900 "
+      >
         <img
           className="h-10 w-10 rounded-full"
           src={ProfileImage}
@@ -56,7 +78,7 @@ const Navbar = () => {
             </Link>
           ))}
         </div>
-      </nav>
+      </motion.nav>
     </Container>
   );
 };
